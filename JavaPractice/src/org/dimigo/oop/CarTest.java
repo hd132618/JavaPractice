acebe416e4cbe8d7989f8e1cdddf3d86f411c111
이영석 <hd132618@users.noreply.github.com>
2015-04-16 10:07:11 +0900

    Update CarTest.java

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -9,7 +9,7 @@ package org.dimigo.oop;
  *    |_ CarTest
  * 
  * 1. 개요 : 
- * 2. 작성일 : 2015. 4. 13.
+ * 2. 작성일 : 2015. 4. 16.
  * </pre>
  *
  * @author     :    이영석
@@ -21,9 +21,35 @@ public class CarTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Car2 car = new Car2("현대자동차", "제네시스", "검정색", 225,50000000);
-		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
-		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		Car3 car = new Car3("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car3 car2 = new Car3("기아자동차", "K7", "흰색", 246);
+		Car3 car3 = new Car3("삼성자동차", "SM7", "회색");
+		
+//		Car2 car = new Car2("현대자동차", "제네시스", "검정색", 225,50000000); 
+//	 	Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000); 
+//		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000); 
+		
+////		   Car car = new Car(); 
+////	           Car car2 = new Car(); 
+////	           Car car3 = new Car(); 
+////	 
+////			car.setCompany("현대자동차"); 
+////			car.setModel("제네시스"); 
+////			car.setColor("검정색"); 
+////			car.setMaxSpeed(225); 
+////     		car.setPrice(50000000); 
+////				 
+////			car2.setCompany("기아자동차"); 
+////	     	        car2.setModel("K7"); 
+////	                car2.setColor("흰색"); 
+////			car2.setMaxSpeed(246); 
+////	                car2.setPrice(40000000); 
+////		
+////			car3.setCompany("삼성자동차"); 
+////			car3.setModel("SM7"); 
+////			car3.setColor("회색"); 
+////		        car3.setMaxSpeed(200); 
+////			car3.setPrice(38000000);
 		
 		System.out.println("<< 자동차 목록 >>");
 		System.out.println("제조사명 : " + car.getCompany());
