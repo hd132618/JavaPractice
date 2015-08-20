52cad6f7ed1ccce072e87a83fd66c87365ed4ca5
Young-Seok LEE <Young-Seok LEE@LeeYoungSeok>
2015-08-20 15:57:08 +0900

    12314

---
+++ src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,25 @@
+package org.dimigo.oop;
+
+public class PiggyBankTest {
+
+	public static void main(String[] args) {
+		FamilyMember I = new FamilyMember("나");
+		FamilyMember father = new FamilyMember("아빠");
+		FamilyMember mother = new FamilyMember("엄마");
+		FamilyMember brother = new FamilyMember("남동생");
+		
+		FamilyMember.printMemberCnt();
+		
+		PiggyBank.putMoney(father, 10000);
+		PiggyBank.putMoney(mother, 5000);
+		PiggyBank.putMoney(I, 2000);
+		PiggyBank.putMoney(brother, 1000);
+		
+		PiggyBank.printBalance();
+		
+		PiggyBank.putMoney(I, 1000);
+		
+		PiggyBank.printBalance();
+	}
+
+}
