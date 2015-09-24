37c3fb64b4dfe86eba83b27aa3ecb36874cbf7f2
이영석 <hd132618@users.noreply.github.com>
2015-09-24 11:51:00 +0900

    Create AgeCheckException.java

---
+++ src/org/dimigo/exception/AgeCheckException.java
@@ -0,0 +1,10 @@
+package org.dimigo.exception;
+
+public class AgeCheckException extends Exception{
+	public AgeCheckException(){
+		
+	}
+	public AgeCheckException(Movie movie) {
+		super(movie.getTitle() + "은/는 " + movie.getLimitAge() + " 이상 관람가 입니다.");
+	}
+}
