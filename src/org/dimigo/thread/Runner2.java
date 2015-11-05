540ccc7f6fa41e6ba4a1a9a4f83b1ff8abab471b
이영석 <hd132618@users.noreply.github.com>
2015-11-05 11:53:26 +0900

    Create Runner2.java

---
+++ src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,21 @@
+package org.dimigo.thread;
+
+public class Runner2 implements Runnable{
+	private String name;
+	
+	public Runner2() {
+		
+	}
+	
+	public Runner2(String name) {
+		this.name = name;
+	}
+	
+	public void run(){
+		System.out.println(name + " 출발");
+		for(int i=10;i>=0;i--){
+			System.out.println(name + " " + i*10 + "미터");
+		}
+		System.out.println(name + " 골인");
+	}
+}
