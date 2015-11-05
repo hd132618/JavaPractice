09526f83e3016f8aa7bde89cd4c339cf1e2acad2
이영석 <hd132618@users.noreply.github.com>
2015-11-05 11:55:40 +0900

    Update Runner2.java

--- src/org/dimigo/thread/Runner2.java
+++ src/org/dimigo/thread/Runner2.java
@@ -15,6 +15,11 @@ public class Runner2 implements Runnable{
 		System.out.println(name + " 출발");
 		for(int i=10;i>=0;i--){
 			System.out.println(name + " " + i*10 + "미터");
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException e){
+				e.printStackTrace();
+			}
 		}
 		System.out.println(name + " 골인");
 	}
